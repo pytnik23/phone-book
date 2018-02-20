28c5fe40c91d7d0a8ff338492b1cf3a982f9904d
Oleksii Teplenko <pytnik.tov@gmail.com>
2018-02-20 17:51:42 +0200

    add gh-pages deploy

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -45,6 +45,7 @@ class ContactForm extends Component {
     };
 
     validateEmail = email => {
+        // eslint-disable-next-line
         const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(email);
     };
